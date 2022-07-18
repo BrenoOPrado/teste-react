e8285af1f44237d3522d55fb4cd5ca749a7518ac
Breno Prado <breno.o.prado@hotmail.com>
2022-07-18 15:05:13 -0300

    Criação do renderWithRouter e inicio do projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Testando o conjunto de links de navegação da aplicação', () => {
+  it('teste do primeiro link', () => {
+    render(<App />);
+    const link = screen.getByRole('link', { name: /Home/i });
+    expect(link).toBeDefined();
+  });
+});
